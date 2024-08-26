144184e1da05b8fbf5314d8349ed8c1353fb3362
andrew <andrewtiema@gmail.com>
2024-08-26 15:28:54 +0300

    Initial Commit

---
+++ RickAndMotryApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMotryApi
+//
+//  Created by Andrew Ananda on 26/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
